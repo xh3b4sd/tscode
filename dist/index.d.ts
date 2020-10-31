69b6676bbfcb0ccecf69e267b85b7e6c19f5d2cd
Tim Schindler <xh3b4sd@gmail.com>
2020-10-31 18:11:40 +0100

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,26 @@
-export { APIClient as Post } from "./pbf/post/ApiServiceClientPb";
-export { APIClient as User } from "./pbf/user/ApiServiceClientPb";
+import * as PostClient from "./pbf/post/ApiServiceClientPb";
+import * as PostCreate from "./pbf/post/create_pb";
+import * as PostDelete from "./pbf/post/delete_pb";
+import * as PostSearch from "./pbf/post/search_pb";
+import * as PostUpdate from "./pbf/post/update_pb";
+export declare const Post: {
+    Client: typeof PostClient.APIClient;
+    CreateI: typeof PostCreate.CreateI;
+    CreateO: typeof PostCreate.CreateO;
+    Delete: typeof PostDelete;
+    Search: typeof PostSearch;
+    Update: typeof PostUpdate;
+};
+import * as UserClient from "./pbf/user/ApiServiceClientPb";
+import * as UserCreate from "./pbf/user/create_pb";
+import * as UserDelete from "./pbf/user/delete_pb";
+import * as UserSearch from "./pbf/user/search_pb";
+import * as UserUpdate from "./pbf/user/update_pb";
+export declare const User: {
+    Client: typeof UserClient;
+    Create: typeof UserCreate;
+    Delete: typeof UserDelete;
+    Search: typeof UserSearch;
+    Update: typeof UserUpdate;
+};
 //# sourceMappingURL=index.d.ts.map
